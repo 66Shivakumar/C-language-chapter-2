07ae90880c16fa1376fd843d0f7fef2e209312c9
66Shivakumar <shiva12109@gmail.com>
2025-10-19 13:37:25 +0530

    Add files via upload

---
+++ 01_probblem.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    // int a = 5656; // not divisible
+    int a = 248999; // divisible
+    printf("result=%d", a%97);
+    return 0;
+}
---
+++ 02_problem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int x = 2;
+    int y = 3; int k = 1;
+    int z = 3;
+    printf("the value is %f", 3*x/y-z+k);
+    return 0;
+}
